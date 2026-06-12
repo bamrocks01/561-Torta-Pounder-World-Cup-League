8162a598984f9a097c463e783a57261fb49fb074
bamrocks01 <37388940+bamrocks01@users.noreply.github.com>
2026-06-12 10:16:49 -0400

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
 import streamlit as st
 
-st.title("World Cup Draft Test")
-st.success("Streamlit is working.")
-
-try:
-    token = st.secrets["FOOTBALL_DATA_TOKEN"]
-    st.write("API key found.")
-except Exception:
-    st.warning("API key not found.")
+st.write("Hello World")
